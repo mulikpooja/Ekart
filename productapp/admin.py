8a8425bd6581b18ef94d3f01f70b549dd55ac165
mulikpooja <poojamulik2075@gmail.com>
2023-06-06 12:05:03 +0530

    First commit

---
+++ productapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from productapp.models import Product,Order,OrderHistory
+# Register your models here.
+#admin.site.register(Product)
+
+class ProductAdminClass(admin.ModelAdmin):
+    list_display=['name','cat','price','status']
+    list_filter=['status','cat']
+
+admin.site.register(Product,ProductAdminClass)
+admin.site.site_header="Ekart Dashboard"
+admin.site.register(Order)
+admin.site.register(OrderHistory)    
